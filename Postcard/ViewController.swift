8146f15e834289c3a21731e5474a6ebe4548f6e3
Edward Clark <eeclark@gmail.com>
2015-01-20 13:44:10 -0600

    added nameLabel Challenge 1

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,11 +12,10 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
-    @IBOutlet weak var nameLabel: UILabel!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
